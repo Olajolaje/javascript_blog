d88bc5938f616242c0b62c798a8fad6f1d5bbdb7
Aleksandra Jedral <aleksandra.jedral@gmail.com>
2020-01-05 12:30:24 +0100

    Project update

--- js/script.js
+++ js/script.js
@@ -1,33 +1,47 @@
-x`'use strict';
+'use strict';
 
 function titleClickHandler(event){
+  event.preventDefault();
+  const clickedElement = this;
   console.log('Link was clicked!');
 
-  /* remove class 'active' from all article links  */
+
+  /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
-for(let activeLink of activeLinks){
-  activeLink.classList.remove('active');
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
+
+
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+  clickedElement.classList.add('active');
+  console.log('clickedElement:', clickedElement);
+
+
+  /* [DONE ???????????????] remove class 'active' from all articles */
 }
-
-  /* add class 'active' to the clicked link */
-
-  /* remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll('.posts .post active .post-content p');
+const activeArticles = document.querySelectorAll('.posts .post active p');
 
 for(let activeArticle of activeArticles){
   activeArticle.classList.remove('post active');
 }
 
-  /* get 'href' attribute from the clicked link */
+/* IN PROGRESS !!!!!!!!!!!!!! get 'href' attribute from the clicked link */
+const articleSelector = href = activeArticle.getAttribute('.posts .post active p');
+console.log('clickedElement');
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+/* find the correct article using the selector (value of 'href' attribute) */
 
-  /* add class 'active' to the correct article */
-}
+/* add class 'active' to the correct article */
 
 const links = document.querySelectorAll('.titles a');
 
-for(let link of links){
+for(let link of links) {
   link.addEventListener('click', titleClickHandler);
+}
+const articles = document.querySelectorAll('.posts p');
+
+for(let article of articles) {
+  article.addEventListener('click', titleClickHandler);
 }
